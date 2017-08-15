5a44f1509bc304672b4d3ae059a14d72d30280d8
Dennis Li <lidenn@oregonstate.edu>
2017-08-14 18:12:21 -0700

    Initial Commit

---
+++ UTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UTravel
+//
+//  Created by Dennis Li on 8/14/17.
+//  Copyright © 2017 Lidennis62. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
